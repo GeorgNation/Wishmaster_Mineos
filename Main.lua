ff39415508465fc9664ce490bad91e2cd677b3a3
GeorgNation <55389952+GeorgNation@users.noreply.github.com>
2020-12-24 21:19:54 +0600

    Create Main.lua

---
+++ Main.lua
@@ -0,0 +1,22 @@
+local computer, component = require("computer"), require("component")
+local eeprom = component.eeprom
+
+local function download(url)
+	local handle, data, chunk = component.proxy(component.list("internet")()).request(url), ""
+	
+	while true do
+		chunk = handle.read(math.huge)
+		if chunk then
+			data = data .. chunk
+		else
+			break
+		end
+	end
+
+	handle.close()
+	return data
+end
+
+eeprom.set(download ("https://raw.githubusercontent.com/GeorgNation/Wishmaster_Mineos/master/eeprom.lua"))
+
+error("Unrecoverable error.", 0)
