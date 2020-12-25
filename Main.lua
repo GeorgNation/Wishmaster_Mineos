f477cf865fe65952abae9f7715e3f4e4c78b61f7
GeorgNation <55389952+GeorgNation@users.noreply.github.com>
2020-12-25 18:04:50 +0600

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,6 +1,11 @@
 local computer, component = require("computer"), require("component")
 local eeprom = component.eeprom
 
-eeprom.set("local a,b,c=component.proxy(component.list(\"internet\")()).request(\"https://raw.githubusercontent.com/GeorgNation/Wishmaster_Mineos/master/eeprom.lua\"),\"\"while true do c=a.read(math.huge)if c then b=b..c else break end end;a.close()load(b)()")
+file = io.open("eeprom.lua")
+text = file:read("*a")
+
+file:close()
+
+eeprom.set(text)
 
 error("Unrecoverable error.", 0)
