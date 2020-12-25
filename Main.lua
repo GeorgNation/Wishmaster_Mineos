5bd9869d0ea417f856799771087965b52c79c346
GeorgNation <55389952+GeorgNation@users.noreply.github.com>
2020-12-25 18:30:45 +0600

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -2,8 +2,10 @@ local computer, component = require("computer"), require("component")
 local eeprom = component.eeprom
 local inet = require("internet")
 
-local response = inet.request("https://raw.githubusercontent.com/GeorgNation/Wishmaster_Mineos/master/eeprom.lua")
-local body = ""
+response = inet.request("https://raw.githubusercontent.com/GeorgNation/Wishmaster_Mineos/master/eeprom.lua")
+body = ""
+chunk = ""
+
 for chunk in response do
   body = body .. chunk
 end
